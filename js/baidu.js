551e7767e049b2fc1e490551e9b89622ae3a84da
120805087 <120805087@qq.com>
2016-11-30 01:56:09 +0800

    baidu

---
+++ js/baidu.js
@@ -0,0 +1,43 @@
+$(function(){
+    // $("#fullpage").mousedown(function(e){
+    //     e.preventDefault();
+    // })
+    // $("#fullpage").mousemove(function(e){
+    //     e.preventDefault();
+    // })
+    var clienH = $(window).height();
+    var num = 0;
+    var flag = true;
+    touch.on("body","swipeup swipedown","#fullpage",function(e){
+        if(e.type == "swipeup"){
+            if(!flag){
+                return;
+            }
+            num++;
+            if(num==$("section").length){
+                num=$("section").length-1;
+                return;
+            }
+            $("#fullpage").css({
+                marginTop:-num*clienH
+            })
+            flag = false;
+        }else if(e.type == "swipedown"){
+            if(!flag){
+                return;
+            }
+            num--;
+            if(num==-1){
+                num=0;
+                return;
+            }
+            $("#fullpage").css({
+                marginTop:-num*clienH
+            })
+            flag = false;
+        }
+    })
+    $("#fullpage")[0].addEventListener("webkitTransitionEnd",function(){
+        flag = true;
+    })
+})
